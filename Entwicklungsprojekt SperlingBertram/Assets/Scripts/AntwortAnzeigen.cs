93102272ef816a7cf24e6f5d60b00919bb514c4a
janinasperling <janinasperling@web.de>
2019-12-08 15:55:39 +0100

    Anklicken der möglichen Antworten und Anzeige der Lösung implementiert

---
+++ Entwicklungsprojekt SperlingBertram/Assets/Scripts/AntwortAnzeigen.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AntwortAnzeigen : MonoBehaviour
+{
+    public GameObject activeBlock;
+    public KartenZiehen skript;
+    public Image rend;
+    void Start()
+    {
+        skript = GetComponent<KartenZiehen>();
+    }
+
+    int[] loesungsarray = new int [] {1, 2, 3, 1};
+    string kartenarray;
+    
+
+    public string KartenarrayBestimmen(){
+
+        if(activeBlock.tag == "gruen"){
+            kartenarray = "GrueneAntworten";}
+            else if(activeBlock.tag == "blau"){
+                kartenarray = "BlaueAntworten";}
+                else if(activeBlock.tag == "rot"){
+                    kartenarray = "RoteAntworten";}
+        else{kartenarray = "SchwarzeAntworten";}
+
+        return kartenarray;
+    }
+
+    public void ButtonOben() {
+        
+        string bestimmtesKartenarray = KartenarrayBestimmen();
+
+        int randomZahl = skript.randomFrage+1;
+
+        if (loesungsarray[skript.randomFrage] == 1){
+            rend.gameObject.SetActive(true);
+        rend.GetComponent<Image>().sprite = Resources.Load<Sprite>("Karten/"+bestimmtesKartenarray+"/"+randomZahl+".1");
+        }
+        else{rend.gameObject.SetActive(true);
+        rend.GetComponent<Image>().sprite = Resources.Load<Sprite>("Karten/"+bestimmtesKartenarray+"/"+randomZahl+".2");}
+    }
+
+    public void ButtonMitte() {
+         string bestimmtesKartenarray = KartenarrayBestimmen();
+
+        int randomZahl = skript.randomFrage+1;
+
+        if (loesungsarray[skript.randomFrage] == 2){
+            rend.gameObject.SetActive(true);
+        rend.GetComponent<Image>().sprite = Resources.Load<Sprite>("Karten/"+bestimmtesKartenarray+"/"+randomZahl+".1");
+        }
+        else{rend.gameObject.SetActive(true);
+        rend.GetComponent<Image>().sprite = Resources.Load<Sprite>("Karten/"+bestimmtesKartenarray+"/"+randomZahl+".2");}
+    }
+
+    public void ButtonUnten() {
+         string bestimmtesKartenarray = KartenarrayBestimmen();
+
+        int randomZahl = skript.randomFrage+1;
+
+        if (loesungsarray[skript.randomFrage] == 3){
+            rend.gameObject.SetActive(true);
+        rend.GetComponent<Image>().sprite = Resources.Load<Sprite>("Karten/"+bestimmtesKartenarray+"/"+randomZahl+".1");
+        }
+        else{rend.gameObject.SetActive(true);
+        rend.GetComponent<Image>().sprite = Resources.Load<Sprite>("Karten/"+bestimmtesKartenarray+"/"+randomZahl+".2");}
+    }
+}
--- Entwicklungsprojekt SperlingBertram/Assets/Scripts/KartenZiehen.cs
+++ Entwicklungsprojekt SperlingBertram/Assets/Scripts/KartenZiehen.cs
@@ -7,74 +7,48 @@ public class KartenZiehen : MonoBehaviour{
 
     //ArrayList für die Spielkarten
     private Sprite[] grueneFragen;
-    private Sprite[] grueneAntworten;
     private Sprite[] blaueFragen;
-    private Sprite[] blaueAntworten;
     private Sprite[] roteFragen;
-    private Sprite[] roteAntworten;
     private Sprite[] schwarzeFragen;
-    private Sprite[] schwarzeAntworten;
     private Sprite[] ereigniskarten;
     public Image rend;
-
     public GameObject activeBlock;
+    public int randomFrage = 0;
+
 
     private void Start (){
 
-        // Assign Renderer component
-       // rend = GetComponent<SpriteRenderer>();
-
         // Karten aus dem Ordner in Array laden 
         grueneFragen = Resources.LoadAll<Sprite>("Karten/GrueneFragen/");
-        grueneAntworten = Resources.LoadAll<Sprite>("Karten/GrueneAntworten/");
         blaueFragen = Resources.LoadAll<Sprite>("Karten/BlaueFragen/");
-        blaueAntworten = Resources.LoadAll<Sprite>("Karten/BlaueAntworten/");
         roteFragen = Resources.LoadAll<Sprite>("Karten/RoteFragen/");
-        roteAntworten = Resources.LoadAll<Sprite>("Karten/RoteAntworten/");
         schwarzeFragen = Resources.LoadAll<Sprite>("Karten/SchwarzeFragen/");
-        schwarzeAntworten = Resources.LoadAll<Sprite>("Karten/SchwarzeAntworten/");
         ereigniskarten = Resources.LoadAll<Sprite>("Karten/Ereigniskarten/");
+
 	}
 
+
     //Frage anzeigen
-    public int FrageAnzeigen(/*Sprite[] kartenarray*/){
+    public void FrageAnzeigen(){
 
         Sprite[] kartenarray;
 
+        //aktives Kartenarray bestimmen
         if(activeBlock.tag == "gruen"){
-            kartenarray = grueneFragen;
-            Debug.Log("Grüne Karte");
-        }else if(activeBlock.tag == "blau"){
-            kartenarray = blaueFragen;
-            Debug.Log("Grüne Karte");}
-        
+            kartenarray = grueneFragen;}
+            else if(activeBlock.tag == "blau"){
+                kartenarray = blaueFragen;}
+                else if(activeBlock.tag == "rot"){
+                    kartenarray = roteFragen;}
+                    else if(activeBlock.tag == "schwarz"){
+                        kartenarray = schwarzeFragen;}
         else{kartenarray = ereigniskarten;}
-
-        int randomFrage = 0;
 
         // Random Zahl von der Kartenmenge generieren
         randomFrage = Random.Range(0, kartenarray.Length);
 
-        // Set sprite to upper face of dice from array according to random value
+        //Karte an Stelle von Objekt Image anzeigen
         rend.gameObject.SetActive(true);
         rend.GetComponent<Image>().sprite = kartenarray[randomFrage];
-        
-
-        return randomFrage;
-    }
-
-    // Bei Mausklick Antwort anzeigen
-    private void OnMouseDown(){
-
-        StartCoroutine("KarteDrehen");
-    }
-
-    private void KarteDrehen(Sprite[] kartenarray, int nummer){
-
-        //rend.sprite = kartenarray[nummer];
-    }
-
-    public void ButtonClicked(){
-        FrageAnzeigen();
     }
 }
